b0ac25ff7c590599ab869364775087515a09bba3
MasonSharpe <115174711+MasonSharpe@users.noreply.github.com>
2022-10-12 09:00:24 -0700

    pickups

---
+++ GroupProject/Assets/C#Scripts/Pickup.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    public string type = "Sword";
+    public int damage = 5;
+    public float swingStartup = 0.5f;
+    public float swingDuration = 0.5f;
+    public int swingSpeed = 15;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        print(Player.damage);
+        int tempDamage = Player.damage;
+        float tempSStartup = Player.swingStartup;
+        float tempSDuration = Player.swingDuration;
+        int tempSSpeed = Player.swingSpeed;
+        Player.damage = damage;
+        Player.swingDuration = swingDuration;
+        Player.swingSpeed = swingSpeed;
+        Player.swingStartup = swingStartup;
+        damage = tempDamage;
+        swingStartup = tempSStartup;
+        swingDuration = tempSDuration;
+        swingSpeed = tempSSpeed;
+    }
+}
--- GroupProject/Assets/C#Scripts/Player.cs
+++ GroupProject/Assets/C#Scripts/Player.cs
@@ -7,7 +7,11 @@ public class Player : MonoBehaviour
     public float speed = 10f;
     public GameObject weapon;
     float timerReload = -1;
-    float timerStartup = -1;
+    float timerStartup = float.PositiveInfinity;
+    public static int damage = 50;
+    public static float swingStartup = 0.5f;
+    public static int swingSpeed = 15;
+    public static float swingDuration = 0.5f;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,7 +23,7 @@ public class Player : MonoBehaviour
     {
         if (Input.GetButtonDown("Fire1") && Time.timeScale != 0)
         {
-            timerStartup = 0.5f;
+            timerStartup = swingStartup;
         }
         float xInput = Input.GetAxis("Horizontal");
         float yInput = Input.GetAxis("Vertical");
@@ -37,14 +41,14 @@ public class Player : MonoBehaviour
             timerStartup = float.PositiveInfinity;
             weapon.GetComponent<SpriteRenderer>().enabled = true;
             weapon.transform.localPosition = Vector3.zero;
-            timerReload = 0.2f;
+            timerReload = swingDuration;
             Vector3 mousePosition = Input.mousePosition;
             mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
             Vector3 diff = mousePosition - weapon.transform.position;
             diff.Normalize();
             float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
             weapon.transform.rotation = Quaternion.Euler(0f, 0f, rot_z - 90);
-            weapon.GetComponent<Rigidbody2D>().velocity = (new Vector2(diff.x, diff.y) * 10) + (moveDirection * speed);
+            weapon.GetComponent<Rigidbody2D>().velocity = (new Vector2(diff.x, diff.y) * swingSpeed) + (moveDirection * speed);
         }
     }
 }
