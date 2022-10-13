50222ff57661dcd238d89f12e8512488ab151343
MasonSharpe <115174711+MasonSharpe@users.noreply.github.com>
2022-10-13 09:21:09 -0700

    signpost

--- GroupProject/Assets/C#Scripts/Signpost.cs
+++ GroupProject/Assets/C#Scripts/Signpost.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Signpost : MonoBehaviour
 {
+    public GameObject player;
+    public GameObject panel;
+    public float close = 2;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,16 @@ public class Signpost : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 playerDir = player.transform.position - transform.position;
+        float playerDist = playerDir.magnitude;
+        playerDir.Normalize();
+        if (playerDist <= close)
+        {
+            panel.GetComponent<Canvas>().enabled = true;
+        }
+        else
+        {
+            panel.GetComponent<Canvas>().enabled = false;
+        }
     }
 }
