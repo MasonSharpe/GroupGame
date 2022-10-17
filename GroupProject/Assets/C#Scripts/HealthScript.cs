67a7a65e25fe730360801c7d57536b3eacb2ac71
MitchellBridges <115174685+MitchellBridges@users.noreply.github.com>
2022-10-17 08:31:00 -0700

    PlayerHealthUIDone

---
+++ GroupProject/Assets/C#Scripts/HealthScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class HealthScript : MonoBehaviour
+{
+    public TextMeshProUGUI HealthText;
+    public GameObject Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+	}
+}
