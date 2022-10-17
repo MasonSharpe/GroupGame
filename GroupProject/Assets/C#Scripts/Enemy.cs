8744ba610bde582c02ad8b67c2b761d7990f8fd6
MasonSharpe <115174711+MasonSharpe@users.noreply.github.com>
2022-10-17 08:58:31 -0700

    big fixes 10/17

--- GroupProject/Assets/C#Scripts/Enemy.cs
+++ GroupProject/Assets/C#Scripts/Enemy.cs
@@ -16,6 +16,7 @@ public class Enemy : MonoBehaviour
     public int health = 10;
     float timerInvincibility = -1;
     public int damage;
+    public GameObject dCollidor;
     // Start is called before the first frame update
     void Start()
     {
